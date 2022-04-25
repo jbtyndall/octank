ce8bcbbc105bef5a477c79b9beef5c82fd6cfb18
John Tyndall <john@tyndall.io>
2022-04-25 10:33:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Octank University</title>
   <meta charset="utf-8">
@@ -8,12 +9,12 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    /* Remove the navbar's default margin-bottom and rounded borders */
     .navbar {
       margin-bottom: 0;
       border-radius: 0;
     }
-    
+
     /* Add a gray background color and some padding to the footer */
     footer {
       background-color: #f2f2f2;
@@ -21,35 +22,39 @@
     }
   </style>
 </head>
+
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#"></a>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"></a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-info-sign"></span> $(hostname -f)</a></li>
+        </ul>
+      </div>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-      	<li><a href="#"><span class="glyphicon glyphicon-info-sign"></span> $(hostname -f)</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+  </nav>
 
-<div class="jumbotron">
-  <div class="container text-center">
-    <h2>Octank University AWS Demo</h2>
+  <div class="jumbotron">
+    <div class="container text-center">
+      <h2>Octank University AWS Demo</h2>
+    </div>
   </div>
-</div>
-  
+
   <?php
+$url = "http://169.254.169.254/latest/meta-data/hostname";
+$hostname = file_get_contents($url);
+
 $url = "http://169.254.169.254/latest/meta-data/instance-id";
 $instance_id = file_get_contents($url);
 
@@ -59,43 +64,30 @@ $availability_zone = file_get_contents($url);
 $url = "http://169.254.169.254/latest/meta-data/ami-id";
 $ami_id = file_get_contents($url);
 
+$url = "http://169.254.169.254/latest/meta-data/local-ipv4";
+$ip_address = file_get_contents($url);
+
 $url = "http://169.254.169.254/latest/user-data";
 $userdata = file_get_contents($url);
 ?>
-  
-  <div class="container-fluid">
-    <ul style="font-size:20vw">
-      <li><strong>Hostname:</strong> <?php echo $hostname; ?></li>
+
+  <div class="container-fluid text-center">
+    <h3>Webserver Details</h3>
+    
+    <ul style="font-size:3vw">
       <li><strong>Instance ID:</strong> <?php echo $instance_id; ?></li>
+      <li><strong>Hostname:</strong> <?php echo $ip_address; ?></li>
+      <li><strong>Hostname:</strong> <?php echo $hostname; ?></li>
       <li><strong>Availability Zone:</strong> <?php echo $availability_zone; ?></li>
     </ul>
   </div>
-  
-<div class="container-fluid text-center">
-    <h3>Available Courses</h3>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Number</th>
-                <th>Title</th>
-                <th>Schedule</th>
-                <th>Credits</th>
-                <th>Seats</th>
-            </tr>
-        </thead>
-        <tbody>
-          <tr>
-            
-          </tr>
-        </tbody>
-    </table>
-</div>
 
-<br><br>
+  <br><br>
 
-<footer class="container-fluid text-center">
-  <p>Footer Text</p>
-</footer>
+  <footer class="container-fluid text-center">
+    <p>Octank University &copy; 2022</p>
+  </footer>
 
 </body>
+
 </html>
